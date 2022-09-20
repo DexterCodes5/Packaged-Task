58ea0f2d7cfd073976e44dfe2ec06a051aa8c4d7
dexterlab_5 <103762164+DexterCodes5@users.noreply.github.com>
2022-09-20 15:57:46 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,11 +3,11 @@
 using namespace std::literals;
 
 int main() {
-	std::packaged_task<void*(int&&)> ptask([](int &&x) { return new int(x); });
+	std::packaged_task<void* (unsigned&&)> ptask([](unsigned&& x) { return new unsigned(x); });
 
 	std::future<void*> fut = ptask.get_future();
 
-	std::thread t(std::move(ptask), 6);   
+	std::thread t(std::move(ptask), 6);
 	std::this_thread::sleep_for(2s);
 	t.join();
 	void* ptr = fut.get();
