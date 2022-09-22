3bc173588d79eaae5bc7befb6b00c34ac342e202
dexterlab_5 <103762164+DexterCodes5@users.noreply.github.com>
2022-09-22 11:12:37 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <future>
+#define _GLIBCXX_USE_NANOSLEEP
 using namespace std::literals;
 
-int main() {
-	std::packaged_task<void*(unsigned&&)> ptask([](unsigned&& x) { return new unsigned(x); });
+void main(int argc, char* argv[]) {
+	std::packaged_task<void* (unsigned&&)> ptask([](unsigned&& x) { return new unsigned(x); });
 
 	std::future<void*> fut = ptask.get_future();
 
-	std::thread t(std::move(ptask), 6);
+	std::thread t(std::move(ptask), 'z');
 	std::this_thread::sleep_for(2s);
 	t.join();
 	void* ptr = fut.get();
